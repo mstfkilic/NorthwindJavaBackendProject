020ff34ab3f6da296be3bb504148fdaa1dc7a8eb
= <=>
2022-01-16 02:48:35 +0300

    NEW INTERFACES ARE ADDED WITH JPA REPOSITORY

--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -3,6 +3,9 @@ package kodlamaio.northwind.business.concretes;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;// beans=proje classı 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
@@ -12,6 +15,7 @@ import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
 import kodlamaio.northwind.core.utilities.results.SuccessResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
+
 
 @Service
 public class ProductManager implements ProductService {
@@ -36,6 +40,47 @@ public class ProductManager implements ProductService {
 		// TODO Auto-generated method stub
 		this.productDao.save(product);
 		return new SuccessResult("Product is added to database.");
+	}
+	
+	@Override
+	public DataResult<Product> getByProductName(String productName) {
+		return new SuccessDataResult<Product>(this.productDao.getByProductName(productName),"Product are listed successfully.");
+	}
+	@Override
+	public DataResult<Product> getByProductNameAndCategory(String productName, int categoryId) {
+		return new SuccessDataResult<Product>(this.productDao.getByProductNameAndCategory_CategoryId(productName,categoryId),"Product are listed successfully.");
+	}
+	@Override
+	public DataResult<List<Product>> getByProductNameOrCategory(String productName, int categoryId) {
+		return new SuccessDataResult<List<Product>>(this.productDao.getByProductNameOrCategory_CategoryId(productName,categoryId),"Products are listed successfully.");
+	}
+	@Override
+	public DataResult<List<Product>> getByCategoryIn(List<Integer> categories) {
+		return new SuccessDataResult<List<Product>>(this.productDao.getByCategory_CategoryIdIn(categories),"Products are listed successfully.");
+	}
+	@Override
+	public DataResult<List<Product>> getByProductNameContains(String productName) {
+		return new SuccessDataResult<List<Product>>(this.productDao.getByProductNameContains(productName),"Products are listed successfully.");
+	}
+	@Override
+	public DataResult<List<Product>> getByProductNameStartsWith(String productName) {
+		return new SuccessDataResult<List<Product>>(this.productDao.getByProductNameStartsWith(productName),"Products are listed successfully.");
+	}
+	@Override
+	public DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId) {
+		return new SuccessDataResult<List<Product>>(this.productDao.getByNameAndCategory(productName,categoryId),"Products are listed successfully.");
+	}
+	@Override
+	public DataResult<List<Product>> getAll(int pageNo, int pageSize) {
+		// TODO Auto-generated method stub
+		Pageable pageable = PageRequest.of(pageNo-1, pageSize);
+		return new SuccessDataResult<List<Product>>(productDao.findAll(pageable).getContent());
+	}
+	@Override
+	public DataResult<List<Product>> getAllSorted() {
+		
+		Sort sort = Sort.by(Sort.Direction.DESC,"productName");
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(sort),"All products are listed by descending");
 	}
 	
 
